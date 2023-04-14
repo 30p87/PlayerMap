909cb97176efd4bc4fbeb39f7bbf7488dd4bb03a
30p87 <30p87@duck.com>
2023-04-14 02:19:37 +0200

    V1.1 - Removed unnecessary "Chunk" variable in EntityInfo Dataclass

--- src/main/java/de/_30p87/playermap/EntityInfo.java
+++ src/main/java/de/_30p87/playermap/EntityInfo.java
@@ -1,6 +1,5 @@
 package de._30p87.playermap;
 
-import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
@@ -10,12 +9,10 @@ import org.bukkit.map.MapCursor;
 
 public class EntityInfo {
     public Location location;
-    public Chunk chunk;
     public String caption;
     public MapCursor.Type mapCursorsType;
     public EntityInfo(Entity entity, FileConfiguration config) {
         location = entity.getLocation();
-        chunk = location.getChunk();
         caption = entity instanceof Player && config.getBoolean("display-player-names") ||
                 !(entity instanceof Player) && config.getBoolean("display-entity-names") ?
                     entity.getName() :
